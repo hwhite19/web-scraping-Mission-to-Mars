a062f8ce6982d95fd904bf8df114f97808b106d9
hwhite19 <henry.d.white@gmail.com>
2022-02-01 12:14:40 -0500

    \

---
+++ Mission_to_Mars/.ipynb_checkpoints/scrape_mars.py
@@ -0,0 +1,22 @@
+#scrape_mars.py REAL DEAL
+#import splinter, BeautifulSoup and chrome driver
+from splinter import Browser
+from bs4 import BeautifulSoup as soup
+#import pandas as pd
+from webdriver_manager.chrome import ChromeDriverManager
+
+#scrape all function
+def scrape_all():
+    print("Scrape All was reached")
+# scrape the mars new page
+
+# scrape the featured image page
+
+# scrape the facts page and get table
+
+# scrape the hemispheres pages
+
+
+# set up as a flask app
+if __name__ == "__main__":
+    print(scrape_all()) 
