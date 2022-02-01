fca1b9b2b484c870552599f888087b97dcb1686d
hwhite19 <henry.d.white@gmail.com>
2022-02-01 15:06:03 -0500

    \

---
+++ app.py
@@ -0,0 +1,33 @@
+from flask import Flask, render_template, redirect, url_for
+from flask_pymongo import PyMongo
+import scrape_mars
+
+app = Flask(__name__)
+
+# use flask pymongo to set up the connection to the database
+app.config["MONGO_URI"] = "mongodb://localhost:27017/mars_data_db"
+mongo = PyMongo(app)
+
+@app.route("/")
+def index():
+    return "You reached the Index"
+
+@app.route("/scrape")
+def scrape():
+    # reference to a database collection (table)
+    marsTable = mongo.db.marsData
+
+    # drop the table if it exists
+    mongo.db.marsData.drop()
+
+    # call scrape mars script
+    mars_data = scrape_mars.scrape_all()
+
+    # take the dictionary and load it into mongoDB
+    marsTable.insert_one(mars_data)
+
+    return mars_data
+
+    
+if __name__ == "__main__":
+    app.run()
--- scrape_mars.py
+++ scrape_mars.py
@@ -2,7 +2,7 @@
 #import splinter, BeautifulSoup and chrome driver
 from splinter import Browser
 from bs4 import BeautifulSoup as soup
-#import pandas as pd
+import datetime as dt
 from webdriver_manager.chrome import ChromeDriverManager
 
 #scrape all function
@@ -22,7 +22,11 @@ def scrape_all():
     # build a dictionary using the information from the scrapes
     marsData = {
         "newsTitle": news_title,
-        "newsParagraph": news_paragraph
+        "newsParagraph": news_paragraph,
+        "featuredImage": scrape_feature_img(browser),
+        "facts": scrape_facts_page(browser),
+        "hemispheres": scrape_hemispheres(browser),
+        "lastUpdated": dt.datetime.now()
     }
 
     # stop the webdriver
@@ -51,13 +55,88 @@ def scrape_news(browser):
     # grabs the paragraph
     news_p = slide_elem.find('div', class_ = 'article_teaser_body').get_text()
 
+    # return the title and pargraph
+    return news_title, news_p
+
 
 # scrape the featured image page
+def scrape_feature_img(browser):
+    # Visit URL
+    url = 'https://spaceimages-mars.com/'
+    browser.visit(url)
+
+    full_image_link = browser.find_by_tag('button')[1]
+    full_image_link.click()
+
+    # Parse the resulting html with soup
+    html = browser.html
+    img_soup = soup(html, 'html.parser')
+
+    # find the image url
+    img_url_rel = img_soup.find('img', class_ = 'fancybox-image').get('src')
+
+    # Use the base url to create an absolute url
+    img_url = f'https://spaceimages-mars.com/{img_url_rel}'
+
+    # return the image url
+    return img_url
 
 # scrape the facts page and get table
+def scrape_facts_page(browser):
+    # Visit URL
+    url = 'https://galaxyfacts-mars.com/'
+    browser.visit(url)
+
+    # Parse the resulting html with soup
+    html = browser.html
+    fact_soup = soup(html, 'html.parser')
+
+    # find the facts location
+    factsLocation = fact_soup.find('div', class_="diagram mt-4")
+    factTable = factsLocation.find('table') # grab the html code for the fact table
+
+    # create an empty string
+    facts = ""
+
+    # add the text to the empty string then return
+    facts += str(factTable)
+
+    return facts
 
 # scrape the hemispheres pages
+def scrape_hemispheres(browser):
+    # base url
+    url = "https://marshemispheres.com/"
+    browser.visit(url)
 
+    # Create a list to hold the images and titles.
+    hemisphere_image_urls = []
+
+    # set up a loop
+    for i in range(4):
+        # loop through each of the pages
+        # hemisphere info dictionary
+        hemisphereInfo = {}
+        
+        # We have to find the elements on each loop to avoid a stale element exception
+        browser.find_by_css('a.product-item img')[i].click()
+        
+        
+        # Next, we find the Sample image anchor tag and extract the href
+        sample = browser.links.find_by_text('Sample').first
+        hemisphereInfo["img_url"] = sample['href']
+        
+        # Get Hemisphere title
+        hemisphereInfo['title'] = browser.find_by_css('h2.title').text
+        
+        # Append hemisphere object to list
+        hemisphere_image_urls.append(hemisphereInfo)
+        
+        # Finally, we navigate backwards
+        browser.back()
+
+        # return the hemisphere urls with the titles
+        return hemisphere_image_urls
 
 # set up as a flask app
 if __name__ == "__main__":
